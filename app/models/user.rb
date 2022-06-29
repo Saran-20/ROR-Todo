2bfc390dc9e158e096bf5ac14aa2a1bec1a6ed67
Saran-20 <Saranyaselv2820@gmail.com>
2022-06-29 15:54:31 +0530

    Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
     def self.overdue
-        where('due_date < ?', Date.today)
+        all.where('due_date < ?', Date.today)
       end
       def self.duetoday
-          where(due_date: Date.today)
+          all.where(due_date: Date.today)
       end
       def self.duelater
-          where('due_date > ?', Date.today)
+          all.where('due_date > ?', Date.today)
       end
       def self.completed
          where(completed: 1)
